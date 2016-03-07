1694d901ae507ec2cd400082fe1eebc20a888dcd
sundebiao <sundebiao@Carol.local>
2016-03-07 11:11:17 +0800

    Add a new project

---
+++ Partner/Partner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Partner
+//
+//  Created by sundebiao on 16/3/7.
+//  Copyright © 2016年 sundebiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Partner/Partner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Partner
+//
+//  Created by sundebiao on 16/3/7.
+//  Copyright © 2016年 sundebiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
